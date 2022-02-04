ae265586d7ba6ac709ce035ac7f5de7010e29212
nick <nwagner566@yahoo.com>
2022-02-04 14:54:57 -0500

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Main.tsx
@@ -0,0 +1,12 @@
+import react, { useState } from "react";
+
+export function Main() {
+  const [] = useState();
+  const handSubmit = () => {};
+
+  return (
+    <div>
+      <h1>Social Spark</h1>
+    </div>
+  );
+}
