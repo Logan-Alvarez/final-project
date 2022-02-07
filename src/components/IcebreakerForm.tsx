fe9934bf361d00ee9596a77b305f1102d30a1eb2
stovet <steven.topalian@gmail.com>
2022-02-07 14:17:11 -0500

    changed input to select drop down

--- src/components/IcebreakerForm.tsx
+++ src/components/IcebreakerForm.tsx
@@ -1,3 +1,4 @@
+import e from "express";
 import { FormEvent, useState } from "react";
 import { IceBreakers } from "../models/Icebreakers";
 
@@ -11,15 +12,26 @@ function IcebreakerForm({onSubmit}: Props) {
 
     function handleFormSubmit(e:FormEvent){
         e.preventDefault();
-        console.log(category)
         onSubmit(category);
+        setCategory("");
     }
 
     return (
         <div className="IcebreakerForm">
             <form onSubmit={handleFormSubmit}>
-                <label htmlFor="categorySearch">Search by category</label>
-                <input type="text" id="categorySearch" onChange={e => setCategory(e.target.value)}/>
+                <label htmlFor="category">Search by category</label>
+               <select name="category" id="category" onChange={e => setCategory(e.target.value)}>
+                   <option value="funny">Funny</option>
+                   <option value="travel">Travel</option>
+                   <option value="animal">Animal</option>
+                   <option value="food">Food</option>
+                   <option value="hobby">Hobby</option>
+                   <option value="haveYou">Have You Ever...</option>
+                   <option value="technology">Technology</option>
+                   <option value="scifi">Sci-Fi &amp; Entertainment</option>
+                   <option value="reflective">Reflective</option> 
+                   <option value="work">Work</option>
+               </select>
                 <button type="submit">Submit</button>
             </form>
         </div>
