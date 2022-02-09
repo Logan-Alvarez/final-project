6ba98beb562a07934a914e977c6719ec4cb0eb69
Logan Alvarez <logan.alvarez616@gmail.com>
2022-02-09 18:46:04 -0500

    Fixed delete api request's

--- src/components/Favorites-Folder/IcebreakerFavorites.tsx
+++ src/components/Favorites-Folder/IcebreakerFavorites.tsx
@@ -1,15 +1,18 @@
 import { IceBreakers } from "../../models/Icebreakers";
+import { deleteFavIcebreakers } from "../../services/icebreaker-api";
 
 interface Prop {
   icebreakers: IceBreakers[];
 }
 
 function IcebreakerFavorites({ icebreakers }: Prop) {
+  function removeFavorite() {}
   return (
     <div className="IcebreakerFavorites">
       {icebreakers.map((icebreaker, i) => (
         <div key={i}>
           <h1>{icebreaker.question}</h1>
+          <button> - Favorites</button>
         </div>
       ))}
     </div>
--- src/models/Joke.ts
+++ src/models/Joke.ts
@@ -1,9 +1,11 @@
+import { ObjectId } from "mongodb";
+
 interface JokeResponse {
   data: jokes[];
 }
 
 export interface jokes {
-  _id?: string;
+  _id?: ObjectId;
   type: string;
   setup: string;
   punchline: string;
