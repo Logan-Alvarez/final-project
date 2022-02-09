1f4564282f988a54f900c9841a1a83cbb8597d00
Logan Alvarez <logan.alvarez616@gmail.com>
2022-02-09 11:09:21 -0500

    Added +Favorites button to Trivia

--- src/components/Trivia-Folder/SingleTrivia.tsx
+++ src/components/Trivia-Folder/SingleTrivia.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Data from "../../models/Trivia";
+import { postFavTrivia } from "../../services/trivia-api";
 
 interface Prop {
   trivia: Data;
@@ -8,22 +9,28 @@ interface Prop {
 function SingleTrivia({ trivia }: Prop) {
   const [hide, setHide] = useState(false);
 
-  function toggle(){
-    if(hide === false){
+  function toggle() {
+    if (hide === false) {
       setHide(true);
-    }else {
+    } else {
       setHide(false);
     }
   }
+
+  function addToFavorites() {
+    postFavTrivia(trivia);
+  }
+
   return (
     <div>
       <h1>Single Trivia</h1>
       <h3>{trivia.question}</h3>
-      {
-        hide ?
-      <h3 onClick={toggle}>{trivia.answer}</h3>:
-      <div onClick={toggle}>Click for Answer</div>
-      }
+      {hide ? (
+        <h3 onClick={toggle}>{trivia.answer}</h3>
+      ) : (
+        <div onClick={toggle}>Click for Answer</div>
+      )}
+      <button onClick={addToFavorites}>+ Favorties</button>
     </div>
   );
 }
