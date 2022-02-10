f3e6901dfa1077fc35e2b7885fbc85978c21d1cd
stovet <steven.topalian@gmail.com>
2022-02-10 08:55:56 -0500

    its a mess but still trying to figure out displaying the icebreakers correctly

--- src/components/Icebreakers-Folder/Icebreakers.tsx
+++ src/components/Icebreakers-Folder/Icebreakers.tsx
@@ -7,25 +7,50 @@ import IcebreakerForm from "./IcebreakerForm";
 export function Icebreakers() {
   //HOOK
   const [icebreakers, setIcebreaker] = useState<IceBreakers[]>([]);
-
+  const [allIcebreakers, setAllIcebreakers] = useState<IceBreakers[]>([]);
+  const [category, setCategory] = useState("");
+  const [count, setCount] = useState(0);
   //ICEBREAKER useEffect.
   useEffect(() => {
     getIceBreakers().then((data) => setIcebreaker(data));
+    getIceBreakers().then((data) => setAllIcebreakers(data));
   }, []);
   // console.log(icebreakers); - RETURNED ARRAY
   function handleIcebreakerSubmit(category: string) {
+    if(count === 0){
     let newArray: IceBreakers[] = icebreakers.filter(icebreaker => icebreaker.category === category);
     setIcebreaker(newArray);
+    // setIcebreaker(allIcebreakers);
+    setCategory(category);
+    setCount(1);
+    console.log('first test');
+    console.log(icebreakers);
+    console.log('test');
+    console.log(allIcebreakers);
+    } else {
+      
+      setIcebreaker(allIcebreakers);
+      console.log(icebreakers);
+      console.log('second test');
+      setCount(0);
+      let newArray: IceBreakers[] = allIcebreakers.filter(icebreaker => icebreaker.category === category);
+      console.log(newArray);
+    setIcebreaker(newArray);
+    setCategory(category);
+    }
   }
 
   return (
     <div className="Icebreakers">
-      <h1>Icebreakers</h1>
       <div>
+      <h1>Icebreakers</h1>
         <a href="/">Back To Home</a>
       </div>
       <IcebreakerForm onSubmit={handleIcebreakerSubmit} />
-      <IcebreakerList icebreakers={icebreakers} />
+      { category.length > 0 ?
+      <IcebreakerList icebreakers={icebreakers}/> :
+     <span></span>
+}
     </div>
   );
 }
