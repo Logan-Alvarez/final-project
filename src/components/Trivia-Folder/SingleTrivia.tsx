2073f31007ebff1daf938c5f13d4b9ea1293c952
Logan Alvarez <logan.alvarez616@gmail.com>
2022-02-15 10:30:59 -0500

    added back in color prop

--- src/components/Trivia-Folder/SingleTrivia.tsx
+++ src/components/Trivia-Folder/SingleTrivia.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Data from "../../models/Trivia";
 import { postFavTrivia } from "../../services/trivia-api";
 
